396e764743ac738b522686f0051e1a2cf55fcbf9
Ryuma-code <alyasanurian@gmail.com>
2025-02-02 14:20:04 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -40,7 +40,7 @@ def weather_pattern_clustering():
     sns.scatterplot(x='temp', y='hum', hue='cluster_label', data=data, palette='viridis')
     st.title('Weather Pattern Clustering')
     st.write('Temperature')
-    st.write('Memperlihatkan kondisi kelembapan,suhu,kecepatan angin')
+    st.write('Memperlihatkan kondisi kelembapan,suhu,kecepatan angin, dan penyewa')
     st.pyplot()
 
 if __name__ == "__main__":
